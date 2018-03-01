b5b3e7fe05685bb75fc979f13937d7da4e4981c5
James Whitney <jamesdrums@mac.com>
2018-03-01 16:09:13 -0500

    chore: Creates new files for sent meme controllers

---
+++ MemeMe/SentMemesCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe
+//
+//  Created by James Whitney on 3/1/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - SentMemesCollectionViewController
+
+class SentMemesCollectionViewController: UICollectionViewController {
+    
+}
