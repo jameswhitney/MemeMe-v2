7f7436d87cc2342c1b2fba5213589dd85484eff5
James Whitney <jamesdrums@mac.com>
2018-03-02 11:54:31 -0500

    feat: Adds Table View Data Source
    SentMemesTableViewController now has all the appropriate functions
    for TableView.

--- MemeMe/SentMemesTableViewController.swift
+++ MemeMe/SentMemesTableViewController.swift
@@ -22,4 +22,34 @@ class SentMemesTableViewController: UITableViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         memes = appDelegate.memes
     }
+    
+    // MARK: Table View Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // Return number of elements in Meme array
+        print(self.memes.count)
+        return self.memes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SentMemesTableViewCell")!
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        
+        
+        cell.imageView?.image = meme.memedImage
+        cell.textLabel?.text = ""
+        
+        // TODO: Set cell text detail labels
+        if let detailTextLabel = cell.detailTextLabel {
+            detailTextLabel.text = "detail"
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 }
