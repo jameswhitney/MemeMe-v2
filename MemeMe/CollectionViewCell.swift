c73a201055a66b84ffa9613ecaa55083c42131f6
James Whitney <jamesdrums@mac.com>
2018-03-06 14:55:40 -0500

    feat: Creates custom cells for sent memes
    Created Cocoa Touch class for CollectionViewCells for customization.
    Adds storyboard identifiers for SentMemeCollectionViewCell and
    SentMemesCollectionViewController.

---
+++ MemeMe/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  MemeMe
+//
+//  Created by James Whitney on 3/6/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    // TODO: Create Custom Cells
+    
+}
---
+++ MemeMe/SentMemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by James Whitney on 3/6/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class SentMemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sentMemeLabel: UILabel!
+    @IBOutlet weak var sentMemeImage: UILabel!
+}
